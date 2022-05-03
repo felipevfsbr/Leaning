0f38195ec1e71644eed7b3b35f360a5607d24623
Felipe Néris <104584505+felipevfsbr@users.noreply.github.com>
2022-05-03 09:56:52 -0300

    Create TesteString.py

---
+++ TesteString.py
@@ -0,0 +1,4 @@
+nome = input('digite seu nome completo:')
+nome1 = nome.strip()
+
+print(f'Seu nome completo é', nome,', Seu nome com letra minuscula é', nome.lower(),', Seu nome tem a quantidade de caracteres:', len(nome.strip()))
